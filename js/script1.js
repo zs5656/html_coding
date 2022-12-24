be99a0a8be7b8f773fd0fb8363cbbcb3f087855e
zs5656 <0613zachary.sng@gmail.com>
2022-12-24 13:37:19 +0800

    Added some javascript stuff

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,21 @@
+#!/c/PROGRA~1/nodejs/node
+//this line lets me run the file in cmd line woo
+
+alert("e");
 console.log("Hello World!", "Hello2");
+
+// declaring variables
+let firstname = "musers";
+let lastname = "fish";
+console.log(firstname + " " + lastname);
+
+// backtick syntax
+console.log(`${firstname} is ${lastname}`);
+
+//giving length of string
+console.log(firstname.length);
+
+// In javscript string 1 == 1.
+// Use triple = so this isn't a problem
+console.log("1" == 1);
+console.log("1" === 1);
---
+++ js/script2.js
@@ -0,0 +1,8 @@
+#!/c/PROGRA~1/nodejs/node
+
+let password = "fossddsdso"
+if (password.length < 6) {
+    console.log("Password is too short.")
+} else {
+    console.log("Password is long enough")
+}
